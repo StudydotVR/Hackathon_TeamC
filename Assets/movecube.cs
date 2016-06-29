83fd0958ae810073d72b248ffda683e4e3eec4e4
MotokiSakata <motoki.8911@gmail.com>
2016-06-29 17:52:08 +0900

    マテリアル変更

---
+++ Assets/movecube.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movecube : MonoBehaviour {
+
+	//public NavMeshAgent agent;
+	//public Animator animator;
+
+	//int beforce;
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log("test");
+		if (other.CompareTag ("cube")) {
+			other.gameObject.SendMessage ("Move", 1f, SendMessageOptions.DontRequireReceiver);
+
+
+		}
+	}
+}
+;
