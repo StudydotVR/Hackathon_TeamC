1e8ffba264622d05181ef5aa852e5812a0ff12bb
takeoinishie <takeoinishie@gmail.com>
2016-06-29 20:11:29 +0900

    ギミック 20:11v
    Signed-off-by: takeoinishie <takeoinishie@gmail.com>

--- Assets/ddst.cs
+++ Assets/ddst.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class ddst : MonoBehaviour {
     int jtest=0;
     GameObject ob;
-    GameObject[] ob2;
+    GameObject ob2;
     // Use this for initialization
     void Start () {
         ob = GameObject.Find("qb");
-        ob2= GameObject.FindGameObjectsWithTag("button");
+        ob2= GameObject.Find("eButton");
     }
 	
 	// Update is called once per frame
@@ -21,21 +21,21 @@ public class ddst : MonoBehaviour {
         */
         if (jtest == 1)
         {
-            float y = 0;
-            for (y = 0; y < 10; y += 0.01f)
-            {
-                gameObject.transform.localPosition=new Vector3(4.37f, 11.06f+y*Time.deltaTime, 10.36f);
-            }
+            float y = gameObject.transform.position.y;
+            y += 0.01f;
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+               
         }
     }
     public void OnClick()
     {
         jtest = 1;
         //ob.gameObject.transform.Translate(up*Time.deltaTime,0,0);
-        for (int i = 0; i < ob2.Length; i++)
-        {
-            ob2[i].SetActive(false);
-        }
+       // for (int i = 0; i < ob2.Length; i++)
+        //{
+            ob2.SetActive(false);
+            Debug.Log("ab");
+        //}
         
 
     }
