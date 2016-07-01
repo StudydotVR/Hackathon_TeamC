8fd2e58dfd80e036da4e687b17970ee613fb71fd
MotokiSakata <motoki.8911@gmail.com>
2016-07-01 17:01:25 +0900

    fall floor作成

---
+++ Assets/_Scrips/Fall.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fall : MonoBehaviour {
+
+	private int flag =0;
+	public GameObject ob; 
+	float pos_y ;
+	//
+
+	void Start(){
+		pos_y = ob.transform.position.y;
+	}
+	void Update () {
+		if (flag == 1) {
+			//float y = gameObject.transform.position.y;
+			pos_y -= 0.01f;
+			transform.position = new Vector3(transform.position.x, pos_y, transform.position.z);
+
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag=="Player"){
+			//setflag ();
+				Invoke ("setflag", 0.5f);
+			//flag = 1;
+		}
+	}
+
+	void setflag(){
+		//Debug.Log ("fall");
+		flag = 1;
+	}
+
+
+}
