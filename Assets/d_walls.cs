f9add844abc759cc16fce70fbea76fce1281a647
takeoinishie <takeoinishie@gmail.com>
2016-07-03 12:01:28 +0900

    12:01v

---
+++ Assets/d_walls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class d_walls : MonoBehaviour {
+   GameObject one;
+    public GameObject wall;
+    //private int num;
+    
+	// Use this for initialization
+	void Start () {
+        one = GameObject.FindGameObjectWithTag("wall");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // highelectricsystem hs= one.GetComponent<highelectricsystem>();
+        if (highelectricsystem.hes == 1)
+        {
+            one.SetActive(false);
+        }
+	}
+}
---
+++ Assets/highelectricsystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class highelectricsystem : MonoBehaviour {
+    public static  int hes;
+    GameObject obj;
+	// Use this for initialization
+	void Start () {
+        obj = GameObject.FindGameObjectWithTag("cloth");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            hes = 1;
+            obj.gameObject.SetActive(false);
+        }
+    }
+    public int get_hes()
+    {
+        return hes;
+    }
+    
+}
